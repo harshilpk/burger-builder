f948767bd34c83190144d052889eba4de978a830
Harshil <harshilpk@gmail.com>
2018-12-23 13:33:44 -0700

    Added the Burger Component. Not adding ingredients dynamically ATM

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import classes from './Layout.css';
+
+const layout = props => (
+  <>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={classes.Content}>{props.children}</main>
+  </>
+);
+
+export default layout;
