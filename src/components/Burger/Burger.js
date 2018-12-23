3987f1c8792c88102359c34cef389826cea88027
Harshil <harshilpk@gmail.com>
2018-12-23 16:01:27 -0700

    Adding ingredients dynamically to the Burger(Map and Reduce)

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Burger.css';
-import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import classes from "./Burger.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
-const burger = (props) => {
-return (
+// transforming an object of key value pairs to an array of ingredients
+const burger = props => {
+  let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey + i} type={igKey} />;
+    });
+  })
+  .reduce((arr, el) => {
+      return arr.concat(el)
+  }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p> Please start adding ingredients!</p>
+  }
+  // console.log(transformedIngredients);
+  return (
     <div className={classes.Burger}>
-        <BurgerIngredient type="bread-top" />
-        <BurgerIngredient type="cheese" />
-        <BurgerIngredient type="meat" />
-        <BurgerIngredient type="bread-bottom" />
-
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
     </div>
-);
+  );
 };
 
 export default burger;
