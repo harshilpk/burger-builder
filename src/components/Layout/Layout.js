6da743c845b89b23c4985745cb6e9e1860a7e7af
Harshil <harshilpk@gmail.com>
2018-12-24 15:37:35 -0700

    Adding Side Drawer Navigation

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 
-import classes from './Layout.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
+import classes from "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = props => (
-  <>
-    <Toolbar />
-    <main className={classes.Content}>{props.children}</main>
-  </>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  };
 
-export default layout;
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+        return {showSideDrawer: !prevState.showSideDrawer}
+    });
+  };
+  render() {
+    return (
+      <>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+        <SideDrawer
+          show={this.state.showSideDrawer}
+          closed={this.sideDrawerClosedHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </>
+    );
+  }
+}
+
+export default Layout;
