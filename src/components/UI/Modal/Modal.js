dfaaa05f3b3e8fb1f0c4308705e3849a1c26c91e
Harshil <harshilpk@gmail.com>
2018-12-25 14:13:48 -0700

    Imporving Performance(Modal.js) and Folder structure(Layout.js)

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 
 import classes from "./Modal.css";
 import Backdrop from "../Backdrop/Backdrop";
 
-const modal = props => (
-  <>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      style={{
-        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-        opcaity: props.show ? "1" : "0"
-      }}
-      className={classes.Modal}
-    >
-      {props.children}
-    </div>
-  </>
-);
+class Modal extends Component {
+  shouldComponentUpdate(nextprops, nextState) {
+    if (nextprops.show !== this.props.show) {
+      return true;
+    }
+    return false;
+  }
 
-export default modal;
+  componentWillUpdate () {
+      console.log('[Modal] WillUpdate');
+  }
+
+  render() {
+    return (
+      <>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opcaity: this.props.show ? "1" : "0"
+          }}
+          className={classes.Modal}
+        >
+          {this.props.children}
+        </div>
+      </>
+    );
+  }
+}
+
+export default Modal;
